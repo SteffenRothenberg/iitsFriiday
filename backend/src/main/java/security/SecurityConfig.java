ed97b205b55da285bb87aa6a4a55546ff6ecf3c5
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2023-05-15 11:45:14 +0200

    added bouncycastle dependency to pom.xml created Seurity Config file in backend

---
+++ backend/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpStrictTransportSecurityDsl;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+
+        return http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(requestHandler))
+                .sessionManagement((session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)))
+                .httpBasic().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/users").authenticated()
+                .requestMatchers("/api/albums").authenticated()
+                .anyRequest().permitAll()
+                /*.and()
+                .formLogin()
+                .and()
+                .logout()*/
+                .and().build();
+    }
+}
